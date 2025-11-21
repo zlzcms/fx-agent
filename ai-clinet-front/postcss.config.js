3b554c50684e0ac104a1a17ffc6031d6a4aea813
zhujinlong <2428887449@qq.com>
2025-11-22 00:04:38 +0800

    env

--- ai-clinet-front/postcss.config.js
+++ ai-clinet-front/postcss.config.js
@@ -1,3 +1,9 @@
+/**
+ * @Author: zhujinlong
+ * @Date:   2025-11-21 23:51:10
+ * @Last Modified by:   zhujinlong
+ * @Last Modified time: 2025-11-22 00:00:58
+ */
 export default {
   plugins: {
     tailwindcss: {},
